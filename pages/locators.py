eb12cc536cad7a2daeb53123827c3abe64374e38
Sasha-Sokolova <mashapiterskaya@yandex.ru>
2021-06-12 11:45:40 +0300

    replaced some methods from main_page to base_page

--- pages/locators.py
+++ pages/locators.py
@@ -15,3 +15,7 @@ class ProductPageLocators():
     name_book = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
     price_of_book = (By.CSS_SELECTOR, '#messages > div:nth-child(3) > div > p > strong')
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, 'div.alertinner strong')
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/main_page.py
+++ pages/main_page.py
@@ -4,7 +4,12 @@ from .locators import MainPageLocators
 from .login_page import LoginPage
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
+    class MainPage(BasePage):
+        def __init__(self, *args, **kwargs):
+            super(MainPage, self).__init__(*args, **kwargs)
+
+
+    """def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
@@ -15,4 +20,4 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
-        #return LoginPage(browser=self.browser, url=self.browser.current_url)
+        #return LoginPage(browser=self.browser, url=self.browser.current_url)"""
